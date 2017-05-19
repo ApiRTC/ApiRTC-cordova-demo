b5b1f8565d5868941526ae221a37cd626ad35d3c
Samuel Liard <samuel.liard@apizee.com>
2017-05-19 09:49:35 +0200

    update for cordova 7

--- init.sh
+++ init.sh
@@ -3,12 +3,12 @@
 npm install xcode
 
 cordova plugin add cordova-plugin-console
-cordova plugin add cordova-custom-config@2.0.3
+cordova plugin add cordova-custom-config
 cordova plugin add cordova-plugin-device
-cordova plugin add https://github.com/BasqueVoIPMafia/cordova-plugin-iosrtc.git#4.0.0
+cordova plugin add cordova-plugin-iosrtc
 cordova plugin add cordova-plugin-media
 cordova plugin add android-camera-permission
-cordova plugin add cordova-plugin-android-permissions@0.10.0
+cordova plugin add cordova-plugin-android-permissions
 
 cordova platform add android
 cordova build android
